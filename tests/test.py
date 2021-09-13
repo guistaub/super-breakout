aa3e9f4fd37d1d9a36b1eff99362132221741910
guistaub <guistaub999@gmail.com>
2021-09-12 22:45:40 -0300

    added module structure to project

---
+++ tests/test.py
@@ -0,0 +1,3 @@
+def test(condition, message):
+    assert condition
+    print("%s test passed!" % message)
--- views/app.py
+++ views/app.py
@@ -1,6 +1,9 @@
 import pygame
-from pygame import event
 from pygame.locals import *
+import os
+import test
+
+#Classe que inicializa o jogo
 
 class App():
 
@@ -8,16 +11,26 @@ class App():
         self.height = 500
         self.width = 500
         self._game_running = True
+        self._caption = "Super PyBreakout!"
+        self.icon = pygame.image.load("assets/icon.jpeg")
 
-    def get_game_status(self):
+    def get_game_status(self) -> bool:
         return self._game_running
     
-    def set_game_status(self, status):
+    def set_game_status(self, status) -> None:
         self._game_running = status
 
-    def run_app(self):
+    def get_screen_caption(self) -> str:
+        return self._caption
+
+    def run_app(self) -> None:
+        os.environ['SDL_VIDEO_CENTERED'] = '1'
         pygame.init()
+        pygame.display.set_caption(self.get_screen_caption())
+        pygame.display.set_icon(self.icon)
         window = pygame.display.set_mode((self.width, self.height))
+
+
 
         while self._game_running:
             event_list = pygame.event.get()
@@ -27,3 +40,13 @@ class App():
             pygame.display.update()
 
         pygame.quit()
+
+
+if __name__ == "__main__":
+    
+    test_app = App()
+
+    test(test_app.get_game_status() == True, "Game Status Getter")
+    test_app.set_game_status(False)
+    test(test_app.get_game_status() == False, "Game Status Setter")
+
