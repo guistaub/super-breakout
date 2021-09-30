8331d5cd39bc07a56733d341fc1997b87571797b
guistaub <guistaub999@gmail.com>
2021-09-30 00:18:39 -0300

    added menu

--- views/GameView.py
+++ views/GameView.py
@@ -2,6 +2,7 @@ from models import Ball, Tile
 import pygame
 from pygame.locals import *
 from controllers import GameStateController
+from .MenuView import MenuView
 
 RED = (255, 0, 0, 255)
 TILE_SPACING = 10
@@ -19,6 +20,7 @@ class GameView:
         pygame.display.set_icon(pygame.image.load("assets/icon.jpeg"))
 
         # State Initialization
+        self.menu = MenuView(self.window)
         self.state = GameStateController(self.width, self.height)
         self.ball = Ball(self.state, self.window, 640, 360)
         self.ball.setColor(255, 0, 0, 255)
@@ -28,12 +30,18 @@ class GameView:
         # Loop
         self.clock = pygame.time.Clock()
         self.running = True
+        self.isPlaying = False
 
     def quitRequested(self):
         self.running = False
 
+    def showMenuRequested(self):
+        self.isPlaying = False
+
+    def playGameRequested(self):
+        self.isPlaying = True
+
     def drawTileGroup(self, numberOfRows, numberOfColumns):
-        # group = []
         tile = None
         top = 0
         left = 0
@@ -43,13 +51,18 @@ class GameView:
             for j in range(numberOfColumns):
                 left += 100 + TILE_SPACING
                 tile = Tile(self.state, self.window, left, top)
-                tile.setColor(0, 255, 0, 255)
+                tile.setColor(0, 200, 0, 255)
                 tile.render()
 
     def processInput(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.quitRequested()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_m:
+                    self.showMenuRequested()
+                elif event.key == pygame.K_p:
+                    self.playGameRequested()
 
     def update(self):
         self.state.updateBallPosition()
@@ -57,8 +70,11 @@ class GameView:
 
     def render(self):
         self.window.fill((0, 0, 0))
-        self.ball.render()
-        self.drawTileGroup(4, 9)
+        if self.isPlaying:
+            self.ball.render()
+            self.drawTileGroup(4, 9)
+        else:
+            self.menu.render()
         pygame.display.update()
 
     def run(self):
